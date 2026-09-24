6fb15bf15521d02f3f73b6119e14dc9fc3f91220
salimburanovich1951-cmd <salimburonovich1951@gmail.com>
2026-09-24 22:39:58 +0500

    first commit

---
+++ script6.py
@@ -0,0 +1,18 @@
+from os import remove
+
+ismlar = ('ali', 'behruz', 'muhammad','sardorbek','asadbek')
+print(ismlar)
+print("Salom Abror, bugun choyxona bormi? Mahmud, choyxonaga boramizmi ")
+sonlar = (10,26, -5,32 )
+print(sonlar)
+a=10
+b=-5
+c=15
+d=30
+print(a+b)
+print(a*c)
+print(b-d)
+print(c+b)
+ismlar =('elbek', 'behruz', 'ozodbek', 'sardorbek', 'bobur', 'shaxboz', 'asadbek','hasan','husan','diyorbek',)
+remove=('bobur')
+print(ismlar)
